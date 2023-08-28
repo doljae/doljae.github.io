1506f2fc231ca0d05e7d482dce8438b6054c5a8a
Seokjae Lee <seok9211@naver.com>
2023-08-29 00:02:32 +0900

    fix footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/doljae/doljae.github.io/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "Zero to Hero",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -90,33 +90,24 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
             title: "More",
             items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/doljae",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://linkedin.com/in/doljae",
+              },
+              {
+                label: "Tistory",
+                href: "https://doljae.tistory.com/",
+              },
               // {
               //   label: "Blog",
               //   to: "/blog",
-              // },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
+              // }
             ],
           },
         ],
