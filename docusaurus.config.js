de36754a06340e68c58aeea4c60c8eaac546889f
Seokjae Lee <seok9211@naver.com>
2023-09-05 15:53:20 +0900

    apply algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/doljae/doljae.github.io/blob/main/",
+              "https://github.com/doljae/doljae.github.io/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -61,64 +61,93 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
-      navbar: {
-        title: "Zero to Hero",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
-        },
-        items: [
-          // {
-          //   type: "docSidebar",
-          //   sidebarId: "tutorialSidebar",
-          //   position: "left",
-          //   label: "Tutorial",
-          // },
-          // { to: "/blog", label: "Blog", position: "left" },
-          // {
-          //   href: "https://github.com/facebook/docusaurus",
-          //   label: "GitHub",
-          //   position: "right",
-          // },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "More",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/doljae",
-              },
-              {
-                label: "LinkedIn",
-                href: "https://linkedin.com/in/doljae",
-              },
-              {
-                label: "Tistory",
-                href: "https://doljae.tistory.com/",
-              },
-              // {
-              //   label: "Blog",
-              //   to: "/blog",
-              // }
-            ],
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+          title: "Zero to Hero",
+          logo: {
+            alt: "My Site Logo",
+            src: "img/logo.svg",
           },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Zero to Hero, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['java', 'kotlin']
-      },
-    }),
+          items: [
+            // {
+            //   type: "docSidebar",
+            //   sidebarId: "tutorialSidebar",
+            //   position: "left",
+            //   label: "Tutorial",
+            // },
+            // { to: "/blog", label: "Blog", position: "left" },
+            // {
+            //   href: "https://github.com/facebook/docusaurus",
+            //   label: "GitHub",
+            //   position: "right",
+            // },
+          ],
+        },
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "More",
+              items: [
+                {
+                  label: "GitHub",
+                  href: "https://github.com/doljae",
+                },
+                {
+                  label: "LinkedIn",
+                  href: "https://linkedin.com/in/doljae",
+                },
+                {
+                  label: "Tistory",
+                  href: "https://doljae.tistory.com/",
+                },
+                // {
+                //   label: "Blog",
+                //   to: "/blog",
+                // }
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Zero to Hero, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+          additionalLanguages: ['java', 'kotlin']
+        },
+        algolia: {
+          // The application ID provided by Algolia
+          appId: '8PAI13JGXI',
+
+          // Public API key: it is safe to commit it
+          apiKey: '504edd96210e44369ac771c3daf88cfc',
+
+          indexName: 'doljaeio',
+
+          // Optional: see doc section below
+          contextualSearch: true,
+
+          // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+          externalUrlRegex: 'external\\.com|domain\\.com',
+
+          // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+          replaceSearchResultPathname: {
+            from: '/docs/', // or as RegExp: /\/docs\//
+            to: '/',
+          },
+
+          // Optional: Algolia search parameters
+          searchParameters: {},
+
+          // Optional: path for search page that enabled by default (`false` to disable it)
+          searchPagePath: 'search',
+
+          //... other Algolia params
+        },
+      }),
 };
 
 module.exports = config;
