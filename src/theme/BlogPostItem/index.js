c870b2b37dbcf65c353dbcbfd550d4a6950d0eae
Seokjae Lee <seok9211@naver.com>
2024-08-25 14:42:04 +0900

    bump dependency version

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import useIsBrowser from "@docusaurus/useIsBrowser";
-import {useBlogPost} from '@docusaurus/theme-common/internal'
 import GiscusComponent from "@site/src/components/Giscus/GiscusComponent";
+import {useBlogPost} from "@docusaurus/plugin-content-blog/client";
 
 export default function BlogPostItemWrapper(props) {
     const {metadata, isBlogPostPage} = useBlogPost()
